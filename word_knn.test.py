a716dd9fdd6fffdf74e240d481dd182e301f52e0
Tobias Norlund <tobias@norlund.se>
2015-11-13 15:46:54 +0100

    Added kNN test in word spaces

---
+++ word_knn.test.py
@@ -0,0 +1,38 @@
+import sys; sys.path.insert(0, "../Attention_RI")
+
+from sklearn.neighbors import NearestNeighbors
+from dictionary import *
+
+## -- DESCRIPTION --------------------------------------
+#
+#   Looks up k nearest neighbors to words in a word space
+#
+## -----------------------------------------------------
+
+## -- CONFIGURATION ------------------------------------
+
+# Which words to load
+word_space = RiDictionary("/media/tobiasnorlund/ac861917-9ad7-4905-93e9-ee6ab16360ad/bigdata/Dump/Wikipedia-100000-2000-2")
+
+# How many neighbours to find
+k = 10
+
+## -----------------------------------------------------
+
+(word_vectors, word_map) = word_space.get_all_word_vectors()
+nbrs = NearestNeighbors(n_neighbors=k, algorithm='brute', metric='cosine').fit(word_vectors)
+
+print "Enter the word to lookup:"
+
+while True:
+
+    test_word = raw_input()
+    test_vec = word_space.get_word_vector(test_word)
+    if test_vec is None:
+        print "Not in dictionary, try again..."
+        continue
+
+    # Fetch neighbours
+    distances, indices = nbrs.kneighbors(test_vec)
+    for i in range(10):
+        print word_map.items()[indices[0][i]][0]
